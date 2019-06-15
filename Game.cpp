aeb959c54951a1ce896827eed14560af3f282359
James <45885505+JNBarr@users.noreply.github.com>
2019-06-15 16:27:03 +0100

    Add files via upload

---
+++ Game.cpp
@@ -0,0 +1,43 @@
+// Alien Game
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int score;
+	double distance;
+	char playAgain;
+	bool sheildsup;
+	short lives, aliensKilled;
+	
+	score 0;
+	distance = 1200.76;
+	playAgain = 'y';
+	sheildsUp = true;
+	lives = 3;
+	aliensKilled 10;
+	
+	double engineTemp = 6572.89;
+	
+	cout << "/nscore: " << score << endl;
+	cout << "distance: " << distance << endl;
+	cout << "playAagain: " << playAagain << endl;
+	// Skipping sheildsUp
+	cout << "lives: " << lives << endl;
+	cout << "aliensKilled: " << aliensKilled << endl;
+	cout << "engineTemp: " << engineTemp << endl;
+	
+	int fuel;
+	count << "\nHow much fuel? " ;
+	cin >> fuel;
+	count << "fuel: " << fuel << endl;
+	
+	typedef unsigned short int ushort;
+	ushort bonus = 10;
+	cout << "\nbonus" << bonus << endl;
+	
+	return 0;
+}
+	
+	
